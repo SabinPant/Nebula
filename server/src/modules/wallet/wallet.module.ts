27acc86c523142f299950d39dcdccb52e365db0e
Sabinpabt23 <sabinpant100@gmail.com>
2026-06-18 20:59:10 +0545

    refactor: remove duplicate RedisClient registration from WalletModule, use global instance

--- server/src/modules/wallet/wallet.module.ts
+++ server/src/modules/wallet/wallet.module.ts
@@ -12,12 +12,11 @@ import { WalletController } from './wallet.controller';
 import { WalletService } from './wallet.service';
 import { WalletRepository } from './wallet.repository';
 import { PrismaService } from '../../core/database/prisma.service';
-import { RedisClient } from '../../core/database/redis.client';
 
 @Module({
   imports: [JwtModule],
   controllers: [WalletController],
-  providers: [WalletService, WalletRepository, PrismaService, RedisClient],
+  providers: [WalletService, WalletRepository, PrismaService],
   exports: [WalletService],
 })
 export class WalletModule {}
