97f6ff5a2fe3529e37fc09dc87b60fb929dcf9d2
SabinPant <sabinpant100@gmail.com>
2026-07-01 14:02:52 +0545

    feat: add broker dashboard controller and update module wiring

--- server/src/modules/broker/broker.module.ts
+++ server/src/modules/broker/broker.module.ts
@@ -2,19 +2,22 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { EventEmitterModule } from '@nestjs/event-emitter';
 import { BrokerController } from './broker.controller';
+import { BrokerDashboardController } from './broker-dashboard.controller';
 import { BrokerService } from './broker.service';
 import { BrokerRepository } from './broker.repository';
+import { BrokerGuard } from './guards/broker.guard';
 import { AuthRepository } from '../auth/auth.repository';
 import { PrismaService } from '../../core/database/prisma.service';
 
 @Module({
   imports: [JwtModule, EventEmitterModule.forRoot()],
-  controllers: [BrokerController],
+  controllers: [BrokerController, BrokerDashboardController],
  providers: [
   BrokerService,
   BrokerRepository,
   AuthRepository,
   PrismaService,
+  BrokerGuard,
 ],
   exports: [BrokerService],
 })
